635035f565ebde99f73bdba9799a6c6725c06d38
MansurKZN <mansur0158@gmail.com>
2022-06-23 14:46:00 +0300

    add new features

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,13 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from app.views import Login, logout_user, Profie, MainPage
+from app.views import Login, logout_user, Profie, MainPage, AdminInfo, TimeControl
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
     path('main/', login_required(MainPage.as_view()), name='main'),
     path('profile/', login_required(Profie.as_view()), name='profile'),
+    path('main/timecontrol', login_required(TimeControl.as_view()), name='timecontrol'),
+    path('admininfo/', staff_member_required(AdminInfo.as_view(), login_url='/login'), name='admin'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
+from datetime import date
+import datetime
+
 from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
+from django.db.models import Sum
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
@@ -13,7 +17,11 @@ class Login(LoginView):
     template_name = 'login.html'
 
     def get_success_url(self):
-        return reverse_lazy('main')
+        user = self.request.user
+        if user.is_superuser:
+            return reverse_lazy('admin')
+        else:
+            return reverse_lazy('main')
 
 def logout_user(request):
     logout(request)
@@ -23,11 +31,23 @@ def logout_user(request):
 class MainPage(View):
 
     def get(self, request):
-        user = request.user.username
+        user = request.user
         projects = list(map(lambda x: x.name, Project.objects.all()))
         engineers = list(map(lambda x: x.full_name, Engineer.objects.all()))
         work_types = list(map(lambda x: x.name, WorkType.objects.all()))
-        return render(request, 'main.html', {"user": user, 'projects': projects, 'engineers': engineers, 'work_types': work_types})
+        arr = []
+        work_reports = WorkReport.objects.filter(manager=user)
+        for report in work_reports:
+            a = []
+            a.append(report.date)
+            a.append(report.project.name)
+            a.append(report.engineer.full_name)
+            a.append(report.work_type.name)
+            a.append(report.period)
+            a.append(report.text)
+            arr.append(a)
+
+        return render(request, 'main.html', {"user": user.username, 'projects': projects, 'engineers': engineers, 'work_types': work_types,  'arr': arr})
 
     def post(self, request):
         user = request.user
@@ -37,16 +57,28 @@ class MainPage(View):
         work_type = WorkType.objects.filter(name=request.POST.get("work_type"))[0]
         work_report = WorkReport(manager=user, date=request.POST.get("date"), project=project, engineer=engineer, work_type=work_type, period=request.POST.get("period"), text=request.POST.get("text"))
         work_report.save()
-        projects = list(map(lambda x: x.name, Project.objects.all()))
-        engineers = list(map(lambda x: x.full_name, Engineer.objects.all()))
-        work_types = list(map(lambda x: x.name, WorkType.objects.all()))
-        return render(request, 'main.html', {"user": user, 'projects': projects, 'engineers': engineers, 'work_types': work_types})
+        return redirect('/main')
+        # projects = list(map(lambda x: x.name, Project.objects.all()))
+        # engineers = list(map(lambda x: x.full_name, Engineer.objects.all()))
+        # work_types = list(map(lambda x: x.name, WorkType.objects.all()))
+        # arr = []
+        # work_reports = WorkReport.objects.filter(manager=user)
+        # for report in work_reports:
+        #     a = []
+        #     a.append(report.date)
+        #     a.append(report.project.name)
+        #     a.append(report.engineer.full_name)
+        #     a.append(report.work_type.name)
+        #     a.append(report.period)
+        #     a.append(report.text)
+        #     arr.append(a)
+        # return render(request, 'main.html', {"user": user, 'projects': projects, 'engineers': engineers, 'work_types': work_types, 'arr': arr})
 
 
 class Profie(View):
 
     def get(self, request):
-        user = request.user.username
+        user = request.user
         form = PasswordChangeForm()
         return render(request, 'profile.html', {"user": user, 'form': form})
 
@@ -69,4 +101,32 @@ class Profie(View):
         return render(request, 'profile.html', {"user": user, 'form': form, 'message': message, 'error': error})
 
 
+class AdminInfo(View):
 
+    def get(self, request):
+        user = request.user
+        return render(request, 'admin.html', {'user': user.username})
+
+
+class TimeControl(View):
+
+    def get(self, request):
+        user = request.user
+        arr = []
+        sum = 0
+        dateC = date.today()
+        date2 = dateC + datetime.timedelta(days=1)
+        projects = []
+        for i in WorkReport.objects.filter(manager=user, date__range=(dateC, date2)):
+            projects.append(i.project)
+
+        for project in list(set(projects)):
+            a = []
+            a.append(project.name)
+            company_sum = WorkReport.objects.filter(manager=user, date__range=(dateC, date2), project=project).aggregate(Sum('period')).get('period__sum')
+            sum += company_sum
+            a.append(company_sum)
+            arr.append(a)
+        a = ['', sum]
+        arr.append(a)
+        return render(request, 'timecontrol.html', {'user': user.username, 'arr': arr})
